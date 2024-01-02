3181686fa85d7d557acb27153261fdb6b1e92445
m-rstewart <mikestewart90@gmail.com>
2024-01-02 12:55:22 -0800

    Create new chi router

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+
+	appRouter := chi.NewRouter()
 }
