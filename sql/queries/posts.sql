817b5fdca37dc2d64e48a51da577fa25a7397d5a
m-rstewart <mikestewart90@gmail.com>
2024-01-14 12:31:18 -0800

    Add query for posts by user and generate code

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -10,4 +10,11 @@ INSERT INTO posts (
   feed_id
 )
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-RETURNING *; 
+RETURNING *;
+
+-- name: GetPostsByUser :many
+SELECT posts.* FROM posts
+JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
